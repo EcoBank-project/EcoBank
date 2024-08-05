0da2cf4cb0de8727d039eef434d328ddd3599ed3
HwanLee358 <clse0286@naver.com>
2024-08-06 00:40:37 +0900

    init

--- ecobank/src/main/java/com/ecobank/app/chat/service/ChatMessageVO.java
+++ ecobank/src/main/java/com/ecobank/app/chat/service/ChatMessageVO.java
@@ -1,5 +1,15 @@
 package com.ecobank.app.chat.service;
 
-public class ChatMessageVO {
+import java.util.Date;
 
+import lombok.Data;
+
+@Data
+public class ChatMessageVO {
+	private int msgNo; 			//메시지 번호
+	private String msgContent;	//메시지 내용
+	private String msgType;		//메시지 타입
+	private Date msgSendTime;   //메시지 전송 시간
+	private int userNo;			//회원 번호
+	private int chatNo;			//채팅방 번호
 }
--- ecobank/src/main/java/com/ecobank/app/chat/service/ChatRoomVO.java
+++ ecobank/src/main/java/com/ecobank/app/chat/service/ChatRoomVO.java
@@ -1,5 +1,12 @@
 package com.ecobank.app.chat.service;
 
-public class ChatRoomVO {
+import java.util.Date;
 
+import lombok.Data;
+
+@Data
+public class ChatRoomVO {
+	private int chatNo;				//채팅방 번호
+	private String chatName;		//채팅방 이름
+	private Date chatCreateTime;	//채팅방 생성시간
 }
