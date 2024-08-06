180959b6b1031920d7f33343392691a89f70507f
daeun <ekdms7059@naver.com>
2024-08-06 11:30:41 +0900

    중간 커밋

--- ecobank/src/main/java/com/ecobank/app/challenge/service/ChallService.java
+++ ecobank/src/main/java/com/ecobank/app/challenge/service/ChallService.java
@@ -1,5 +1,18 @@
 package com.ecobank.app.challenge.service;
 
-public interface ChallService {
+import java.util.List;
 
+public interface ChallService {
+	//챌린지 전체 조회
+	public List<ChallVO> challList();
+	
+	//챌린지 단건 조회
+	
+	
+	//챌린지 등록
+	public int challInsert(ChallVO challVO);
+	
+	//챌린지 수정
+	
+	
 }
--- ecobank/src/main/java/com/ecobank/app/challenge/web/ChallengController.java
+++ ecobank/src/main/java/com/ecobank/app/challenge/web/ChallengController.java
@@ -1,8 +1,36 @@
 package com.ecobank.app.challenge.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ecobank.app.challenge.service.ChallService;
+import com.ecobank.app.challenge.service.ChallVO;
 
 @Controller
 public class ChallengController {
-
+	private ChallService challService;
+	
+	@Autowired
+	public ChallengController(ChallService challService) {
+		this.challService = challService;
+	}
+	
+	//챌린지전체조회
+	@GetMapping("ready")
+	public String challList(Model model) {
+		List<ChallVO> list = challService.challList();
+		model.addAttribute("challList", list);
+		return "chall/ready";
+	}
+	
+	//챌린지 등록
+	@GetMapping("challInsert")
+	public String challInsertForm() {
+		return "admins/challInsert";
+	}
+	
 }
