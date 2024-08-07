2edde1dd0f6ef2c257c70c2a5ff1cd2a828656f6
zbvmf530 <zbvmf530@gmail.com>
2024-08-07 18:39:24 +0900

    전체 머지

--- ecobank/src/main/java/com/ecobank/app/security/config/SpringSecurityConfig.java
+++ ecobank/src/main/java/com/ecobank/app/security/config/SpringSecurityConfig.java
@@ -24,7 +24,7 @@ public class SpringSecurityConfig {
         http
         .authorizeHttpRequests(authorize -> authorize
             .antMatchers("/css/**", "/js/**", "/img/**").permitAll() // 정적 자원 허용
-            .antMatchers("/", "/login*", "/signup","/user/**").permitAll() // 인증 필요 없는 경로
+            .antMatchers("/", "/login*", "/signup","/user/**","/about").permitAll() // 인증 필요 없는 경로
             .anyRequest().authenticated() // 나머지 요청은 인증 필요
         )
         .formLogin(formLogin -> formLogin
