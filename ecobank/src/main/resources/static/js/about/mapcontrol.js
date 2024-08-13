7a952ad7b66912b2a41876b2e203e13cbbb3083e
zbvmf530 <zbvmf530@gmail.com>
2024-08-13 17:32:35 +0900

    지도 기능 구현 완료

--- ecobank/src/main/resources/static/js/about/mapcontrol.js
+++ ecobank/src/main/resources/static/js/about/mapcontrol.js
@@ -29,6 +29,10 @@ const MAX_EMISSIONS = 50000000.0; // 최대 탄소배출량 (톤 CO2)
 const MIN_EMISSIONS = 0.0;        // 최소 탄소배출량 (톤 CO2)
 const MAX_SEA_LEVEL = 5.0;        // 최대 해수면 높이 (미터)
 
+// 라디오 버튼별 액션코드 정의 
+const AIR_POLLUTION='air';
+const SEA_LEVEL='sea';
+
 let dataAry = []; // 맵 태그, 테이블 정보 함께 담을 종합 배열
 
 // 화면에서 사용될 표현 데이터 정의(탄소배출량 단계, 단계별 색상표)
@@ -39,7 +43,13 @@ let colorsets = ['#ff0303', '#ff3903', '#ff8103', '#ffe203', '#9aff03', '#35ff03
 const sealabels = [3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0.5, 0.1];
 let seacolorset=['#00008B','#0000CD','#4682B4','#1E90FF','#87CEEB','#008B8B','#20B2AA','#40E0D0'];
 
+// 대기오염도(왼쪽부터 옅은색)
+const airlabels = ['0 - 5 μg/m³', '5.1 - 10 μg/m³', '10.1 - 15 μg/m³', '15.1 - 25 μg/m³', '25.1 - 35 μg/m³', '35.1 - 50 μg/m³', '> 50.1 μg/m³'];
+let aircolorset=['#60ACCB','#9CD84E','#FACF39','#F99049','#F65E5F','#A070B6','#A06A7B'];
+
 let latestClickedRegion;
+
+// 탄소배출량 -> 해수면(m) 변환 함수
 function calculateSeaLevel(emissions) {
 	if (emissions < MIN_EMISSIONS) emissions = MIN_EMISSIONS;
 	if (emissions > MAX_EMISSIONS) emissions = MAX_EMISSIONS;
@@ -49,7 +59,7 @@ function calculateSeaLevel(emissions) {
     return parseFloat(seaLevel.toFixed(2));
 }
 
-// 변환 데이터를 8단계로 변환(색상표)
+// 해수면 변환 데이터를 8단계로 변환(색상표)
 function determineColorCategory(seaLevel) {
    if (seaLevel >= 0 && seaLevel < 0.1) {
         return 7; // 0 ~ 0.09
@@ -70,6 +80,34 @@ function determineColorCategory(seaLevel) {
     }
 }
 
+// 탄소배출량 -> 대기오염도(μg/m³) 변환 함수
+function calculateAirPollution(emissions) {
+    if (emissions < MIN_EMISSIONS) emissions = MIN_EMISSIONS;
+    if (emissions > MAX_EMISSIONS) emissions = MAX_EMISSIONS;
+
+    const pollutionLevel = (emissions - MIN_EMISSIONS) / (MAX_EMISSIONS - MIN_EMISSIONS) * 50;
+    
+    return parseFloat(pollutionLevel.toFixed(1));
+}
+
+// 대기 오염도 변환 데이터를 7단계로 변환(색상표)
+function determinePollutionCategory(pollutionLevel) {
+    if (pollutionLevel >= 0 && pollutionLevel <= 5) {
+        return 1; // 0 - 5 μg/m³
+    } else if (pollutionLevel > 5 && pollutionLevel <= 10) {
+        return 2; // 5.1 - 10 μg/m³
+    } else if (pollutionLevel > 10 && pollutionLevel <= 15) {
+        return 3; // 10.1 - 15 μg/m³
+    } else if (pollutionLevel > 15 && pollutionLevel <= 25) {
+        return 4; // 15.1 - 25 μg/m³
+    } else if (pollutionLevel > 25 && pollutionLevel <= 35) {
+        return 5; // 25.1 - 35 μg/m³
+    } else if (pollutionLevel > 35 && pollutionLevel <= 50) {
+        return 6; // 35.1 - 50 μg/m³
+    } else {
+        return 7; // > 50.1 μg/m³
+    }
+}
 
 function setMapLabel(labels, colorsets) {
 	
@@ -136,9 +174,11 @@ function setAllDataPath() {
 							   code: ditem['nationCode'], 
 							   tag: item, 
 							   emission: ditem['carbonEmissions'],
-							   sealevel:  calculateSeaLevel(ditem['carbonEmissions']),
 							   colorset: colorsets[colorOption] ,
-							   seacolorset : seacolorset[determineColorCategory(calculateSeaLevel(ditem['carbonEmissions']))]
+							   sealevel:  calculateSeaLevel(ditem['carbonEmissions']),
+							   seacolorset : seacolorset[determineColorCategory(calculateSeaLevel(ditem['carbonEmissions']))],
+							   airlevel:calculateAirPollution(ditem['carbonEmissions']),
+							   aircolorset: aircolorset[determinePollutionCategory(calculateAirPollution(ditem['carbonEmissions']))]
 							   });
 			}
 		});
@@ -153,25 +193,64 @@ function setAllDataPath() {
 	// simplemaps api에서 제공하는 이벤트 핸들러에 각각 필요한 함수 등록
 	
 	// 대륙을 선택했을때
-	simplemaps_worldmap.hooks.zoomable_click_region = function(id){focusToRegion(id)};
+	simplemaps_worldmap.hooks.zoomable_click_region = function(id){
+		focusToRegion(simplemaps_worldmap_mapdata.regions[id].name,SEA_LEVEL)
+		$('#flexRadioDefault1').prop('checked', true);
+		};
 	
 	// 뒤로가기 버튼 선택했을때 
 	simplemaps_worldmap.hooks.back = function(){drawWorldMap()};
 	
 	// 나라를 선택했을때
 	simplemaps_worldmap.hooks.click_state = function(id) {
-		alert(id);
+		swal({
+                title: "Coming soon!",
+                icon: "success",
+                buttons: {
+	                    confirm: {
+	                        text: "OK",
+	                        value: true,
+	                        visible: true,
+	                        className: "btn btn-primary",
+	                        closeModal: true
+	                    }
+                    }
+            });
+		
 	}
+	
+	
+	// 라디오 버튼 체크 상태가 변경될 때 실행되는 핸들러 등록
+    $('input[name="flexRadioDefault"]').change(function() {
+        if (this.id === 'flexRadioDefault1') {
+            // Sea Level ratio가 선택되었을 때 실행할 코드
+            focusToRegion(latestClickedRegion,SEA_LEVEL);
+        } else if (this.id === 'flexRadioDefault2') {
+            // Air Pollution ratio가 선택되었을 때 실행할 코드
+            focusToRegion(latestClickedRegion,AIR_POLLUTION);
+        }
+    });
 }
 
 
-function focusToRegion(id){
-	latestClickedRegion = simplemaps_worldmap_mapdata.regions[id].name;
+
+function focusToRegion(regionName,actionCode){
+	// regionCode로부터 region 이름 추출
+	latestClickedRegion = regionName;
+	
 	// dataAry 배열에서 region 필드가 id와 일치하는 객체들만 필터링
     let filteredData = dataAry.filter(item => item.region === latestClickedRegion);
     
-    
-    filteredData.forEach(item=>simplemaps_worldmap_mapdata.state_specific[item.code].color = item.seacolorset);
+    if(actionCode === SEA_LEVEL){
+		filteredData.forEach(item=>simplemaps_worldmap_mapdata.state_specific[item.code].color = item.seacolorset);
+		setMapLabel(sealabels,seacolorset);
+    	$("h6[id='datatype']").text('data: sea level(m)');
+	}
+    else{
+		filteredData.forEach(item=>simplemaps_worldmap_mapdata.state_specific[item.code].color = item.aircolorset);
+		setMapLabel(airlabels,aircolorset);
+    	$("h6[id='datatype']").text('data: PM2.5(μg/m³)');
+	}
     simplemaps_worldmap.refresh();
     //;
     // 화면 
@@ -179,8 +258,7 @@ function focusToRegion(id){
     // 라디오박스 div 표시
     // 라디오박스가 변경될때 각각 표현해줘야할 데이터가 다르기 때문에 기능 분리해야함.
     
-    // 해당 리전에 대해 표현해줘야할 데이터
-    // 1. 탄소배출량(공통)
+    // 1. 해당 region 총 탄소배출량(공통)
     let totalEmissions = filteredData.reduce((accumulator, currentValue) => 
     										(accumulator + currentValue.emission)
     										, 0);
@@ -188,14 +266,17 @@ function focusToRegion(id){
 	// 지도위에 표시해줄 label 설정
 	$("span[id='regionName']").text(latestClickedRegion);
 	$("span[id='totalEmission']").text((totalEmissions+'').replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ","));
+	
+	// 해당 region 총 탄소배출량 레이블 활성화
 	$("div[id='regionDataDiv']").css('display','block');
+	
+	
+	// 사용자 선택 메뉴 활성화
+	$("div[id='dataSelectTab']").css('display','block');
 	
     // 2. 해수면이면 해수면
     // 3. 대기오염도면 대기오염도
     // 2,3번에 해당하는 legend label
-    
-    setMapLabel(sealabels,seacolorset);
-    $("h6[id='datatype']").text('data: sea level(m)');
 }
 
 // 세계 지도 그리는 함수
@@ -204,6 +285,16 @@ function drawWorldMap(){
 	simplemaps_worldmap.refresh();
 	console.log("worldmap Called! " + latestClickedRegion);
 	setMapLabel(carbonlabels,colorsets);
+	
+	
 	$("h6[id='datatype']").text('data: carbon emission(TonCO2/kWh)');
+	
+	// 지역별 탄소배출량 레이블 비활성화
 	$("div[id='regionDataDiv']").css('display','none');
+	
+	// 사용자 선택 메뉴 비활성화
+	$("div[id='dataSelectTab']").css('display','none');
 }
+
+
+
