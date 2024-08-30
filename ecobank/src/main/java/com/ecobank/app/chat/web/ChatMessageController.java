64c7b0e0efabd0c646ea8b7154d5702c19e7574f
HwanLee358 <clse0286@naver.com>
2024-08-31 00:45:02 +0900

    init

--- ecobank/src/main/java/com/ecobank/app/chat/web/ChatMessageController.java
+++ ecobank/src/main/java/com/ecobank/app/chat/web/ChatMessageController.java
@@ -75,9 +75,13 @@ public class ChatMessageController {
 		for(String receiverId : receiverIds) {
 			// 메시지 번역
 			String lagCode = chatService.laguageCodeSelect(receiverId);
-			String translatedText = chatService.translateMessage(message.getMsgContent(), lagCode);
-			String decodedText = decodeHtmlEntities(translatedText);
-			message.setMsgContent(decodedText);
+			String translatedText = null;
+			String decodedText = null;
+			if(message.getMsgContent() != null) {
+				translatedText = chatService.translateMessage(message.getMsgContent(), lagCode);
+				decodedText = decodeHtmlEntities(translatedText);
+				message.setMsgContent(decodedText);				
+			}
 			//닉네임
 			translatedText = chatService.translateMessage(message.getNickName(), lagCode);
 			decodedText = decodeHtmlEntities(translatedText);
